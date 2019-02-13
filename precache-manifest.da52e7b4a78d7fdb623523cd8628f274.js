4d2ab566a8a1dc9d0d4f1f2705c5bdba4d5d8573
Morten Ambrosius <maa@idealdev.dk>
2019-02-13 23:43:07 +0100

    Updates

---
+++ precache-manifest.da52e7b4a78d7fdb623523cd8628f274.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc09e3c9cb63fbf8f097",
+    "url": "/github-pages-test/static/js/runtime~main.cc09e3c9.js"
+  },
+  {
+    "revision": "4aa66fa9e12a3c5c636f",
+    "url": "/github-pages-test/static/js/main.4aa66fa9.chunk.js"
+  },
+  {
+    "revision": "f5ae2494a8dbd9f95f4b",
+    "url": "/github-pages-test/static/js/1.f5ae2494.chunk.js"
+  },
+  {
+    "revision": "4aa66fa9e12a3c5c636f",
+    "url": "/github-pages-test/static/css/main.2520faa9.chunk.css"
+  },
+  {
+    "revision": "89bd3ca9565f4cbef4f4d63ac70e24ab",
+    "url": "/github-pages-test/index.html"
+  }
+];
